96ef24da30384db8ffbea08f9ebce7a651295c0f
Adam Schoolcraft <schoolcraft.adam@yahoo.com>
2022-02-03 20:20:54 -0600

    initial commit spring initializr

---
+++ src/main/java/com/ateam/devinboundportalapi/DevInboundPortalApiApplication.java
@@ -0,0 +1,13 @@
+package com.ateam.devinboundportalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevInboundPortalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevInboundPortalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ateam/devinboundportalapi/DevInboundPortalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ateam.devinboundportalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevInboundPortalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
